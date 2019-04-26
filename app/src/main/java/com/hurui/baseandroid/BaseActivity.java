ea2d6a2f2e1da81ea94d0539e6763bd3a8f84410
hurui <hurui19900515@gmail.com>
2019-04-26 21:26:58 +0800

    修改基类名称

--- app/src/main/java/com/hurui/baseandroid/BaseActivity.java
+++ app/src/main/java/com/hurui/baseandroid/BaseActivity.java
@@ -3,11 +3,14 @@ package com.hurui.baseandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * @author hurui
+ */
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_base);
     }
 }
